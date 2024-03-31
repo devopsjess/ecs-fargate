7d749559da2cb6f9dfeef2bb9b93fd8d089990f6
Hnin <hwutyee@gmail.com>
2024-03-31 21:47:54 +0800

    Second commit

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "ecs-alb" {
-  name               = "luqman-ecs-alb"
+  name               = "wutyee-ecs-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [var.sg_id]
--- ecs.tf
+++ ecs.tf
@@ -2,47 +2,34 @@
 # HINT: if you would like to use your own docker image, update the container_definitions image_url
 # accordingly
 
-
 resource "aws_ecs_task_definition" "own_task_definition" {
-  family                = "luqmanecstaskdef" # Update accordingly
+  family                = "wutyeeesttaskdef" # Update accordingly
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   execution_role_arn        = var.ex_role_arn
   cpu                   =   2048
   memory                = 4096
 
-  # For custom ECR image
-  # container_definitions = templatefile("./files/task-definition.json", {
-  #   image_url        = "255945442255.dkr.ecr.us-east-1.amazonaws.com/luqman-test-ecr-repo:latest"
-  #   container_name   = "luqman-test-ecs"
-  #   port_name        = "luqman-test-ecs-8080-tcp"
-  #   log_group_region = "us-east-1"
-  #   log_group_name   = "/ecs/luqmantesttaskdef"
-  #   log_group_prefix = "ecs"
-  # })
-
-  # For nginx image
   container_definitions = templatefile("./files/task-definition.json", {
-    image_url        = "nginx:latest"
-    port_name        = "nginx80-tcp"
-    container_name   = "NGINX"
-    port_name        = "luqman-test-ecs-8080-tcp"
+    image_url        = "255945442255.dkr.ecr.us-east-1.amazonaws.com/wutyee-test-ecr-repo:latest"
+    container_name   = "wutyee-test-ecs"
+    port_name        = "wutyee-test-ecs-8080-tcp"
     log_group_region = "us-east-1"
-    log_group_name   = "/ecs/luqmantesttaskdef"
+    log_group_name   = "/ecs/wutyeeesttaskdef"
     log_group_prefix = "ecs"
   })
 }
 
-# Creates an ecs clustersctp-ce5-tfstate-bucket-1
+# Creates an ecs cluster
 
 resource "aws_ecs_cluster" "own_cluster" {
-  name = "luqman-ecs-test-cluster" # Update accordingly
+  name = "wutyee-ecs-test-cluster" # Update accordingly
 }
 
 # Creates an ecs service
 
 resource "aws_ecs_service" "own_service" {
-  name             = "luqman-ecs-service" # Update accordingly
+  name             = "my-ecs-service" # Update accordingly
   cluster          = aws_ecs_cluster.own_cluster.arn
   task_definition  = aws_ecs_task_definition.own_task_definition.arn
   desired_count    = 1
@@ -52,12 +39,6 @@ resource "aws_ecs_service" "own_service" {
   deployment_circuit_breaker {
     enable          = true
     rollback        = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.ecs-alb-tg.arn
-    container_name   = "NGINX"
-    container_port   = 80
   }
 
   network_configuration {
